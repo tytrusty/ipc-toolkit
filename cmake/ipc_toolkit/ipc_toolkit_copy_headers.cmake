bafe20844c88edb599fb3d643fb37068ab0987bc
ty <tytrusty@utexas.edu>
2022-08-29 22:06:23 -0400

    Fix ipc_toolkit_copy_headers cmake file

--- cmake/ipc_toolkit/ipc_toolkit_copy_headers.cmake
+++ cmake/ipc_toolkit/ipc_toolkit_copy_headers.cmake
@@ -2,7 +2,7 @@
 function(ipc_toolkit_copy_headers)
   foreach(filepath IN ITEMS ${ARGN})
     file(RELATIVE_PATH filename "${IPC_TOOLKIT_SOURCE_DIR}" "${filepath}")
-    if(${filename} MATCHES ".*\.(hpp|h|ipp|tpp)$")
+    if(${filename} MATCHES ".*\\.(hpp|h|ipp|tpp)$")
       configure_file(${filepath} ${PROJECT_BINARY_DIR}/include/ipc/${filename})
     endif()
   endforeach()
